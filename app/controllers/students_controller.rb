3667a0dc60f0412688c2740ed903a4adadc9e50a
Devin Mullin <devin.mullin90@gmail.com>
2021-11-01 18:01:34 -0500

    commit tree 1f7d6852418ad6ef46d43088869baa1c9881e82b

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,16 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = if params[:name]
+      students = Student.all.select { |student| student.first_name.downcase.include?(params[:name].downcase) || student.last_name.downcase.include?(params[:name].downcase)}
+    else
+      Student.all
+    end 
+    render json: students
+  end
+
+  def show
+    students = Student.find(params[:id])
     render json: students
   end
 
